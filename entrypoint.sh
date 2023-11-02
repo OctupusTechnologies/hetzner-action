80a2ea04fd1d8e845dcc0b9a2e94ecef20cfa5e2
sergiogonzalez <sergio.gonzalez@octupus.es>
2023-11-02 15:53:27 +0100

    create hcloud DNS

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,25 +6,45 @@ HCLOUD_TOKEN=$3
 HCLOUD_SSH_KEY=$4
 HCLOUD_DNS_ZONE=$5
 HCLOUD_FIREWALL=$6
+HETZNER_DNS_TOKEN=$7
 
 echo "Set ENV VAR HCLOUD_TOKEN"
 export HCLOUD_TOKEN=$HCLOUD_TOKEN
 
 hcloud server list | grep $SERVER_NAME
-if [ $? -eq 0 ]; then
+while [ $? -eq 0 ] ; do
   echo "Server $SERVER_NAME already exists"
-  exit 1
-fi
+  SERVER_NAME=$SERVER_NAME-$(date +%s)
+  echo "Try to create server $SERVER_NAME"
+  hcloud server list | grep $SERVER_NAME
+done
 
-echo "Save SSH Key"
-echo $HCLOUD_SSH_KEY > /tmp/hcloud_ssh_key
-chmod 600 /tmp/hcloud_ssh_key
+backup_id=$(hcloud image list | grep 'backup' | awk '{print $1}' | tail -n 1)
+echo "LAST Backup ID: $backup_id"
 
 
 echo "Create server $SERVER_NAME"
-hcloud server create --image ubuntu-20.04 --name $SERVER_NAME --type $SEVER_TYPE --firewall Web --datacenter nbg1-dc3 --ssh-key $HCLOUD_SSH_KEY
+echo "SERVER_NAME: $SERVER_NAME" >> $GITHUB_OUTPUT
+output=$(hcloud server create --image $backup_id --name $SERVER_NAME --type $SEVER_TYPE --firewall Web --datacenter nbg1-dc3 --ssh-key $HCLOUD_SSH_KEY)
+echo $output
 
-
+SERVER_IPV4=$(echo "$output" | awk '/IPv4:/ {print $2}')
+echo "Server IPv4: $SERVER_IPV4"
+echo "SERVER_IPV4: $SERVER_IPV4" >> $GITHUB_OUTPUT
+echo "Create DNS entry for $SERVER_NAME"
+# Create Record
+# Creates a new record.
+response=$(curl -X "POST" "https://dns.hetzner.com/api/v1/records" \
+     -H 'Content-Type: application/json' \
+     -H 'Auth-API-Token: '$HETZNER_DNS_TOKEN'' \
+     -d $'{
+  "value": "'$SERVER_IPV4'",
+  "ttl": 86400,
+  "type": "A",
+  "name": "'$SERVER_NAME'",
+  "zone_id": "'$HCLOUD_DNS_ZONE'"
+}')
+echo "$response"
 
 
 #hcloud context create my-hcloud-context --token $1
