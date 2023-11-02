34a7e26a2eb6910920154b22375670119d80533e
sergiogonzalez <sergio.gonzalez@octupus.es>
2023-11-02 13:03:06 +0100

    config hcloud

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh -l
 # hcloud context create my-hcloud-context --token $HCLOUD_TOKEN
-echo "Comand 1 $1"
-echo "Comand 2 $2"
-echo "Comand 3 $3"
-echo "Comand 4 $4"
-echo "Comand 5 $5"
-echo "Comand 6 $6"
+SEVER_TYPE=$1
+SERVER_NAME=$2
+HCLOUD_TOKEN=$3
+HCLOUD_SSH_KEY=$4
+HCLOUD_DNS_ZONE=$5
+HCLOUD_FIREWALL=$6
+
+echo "Set ENV VAR HCLOUD_TOKEN"
+export HCLOUD_TOKEN=$HCLOUD_TOKEN
+
+hcloud server list
+
+
+
+
 #hcloud context create my-hcloud-context --token $1
 
